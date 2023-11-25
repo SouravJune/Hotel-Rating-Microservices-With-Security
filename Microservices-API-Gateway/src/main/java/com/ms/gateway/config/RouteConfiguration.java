d91df7be9e38bf5c792e79c8be5a51e5985c6c06
SouravJune <106313049+SouravJune@users.noreply.github.com>
2023-11-25 14:11:46 +0530

    changes in application.yml files

--- Microservices-API-Gateway/src/main/java/com/ms/gateway/config/RouteConfiguration.java
+++ Microservices-API-Gateway/src/main/java/com/ms/gateway/config/RouteConfiguration.java
@@ -5,12 +5,18 @@ import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+
+import static org.springframework.http.HttpMethod.GET;
 
 @Configuration
 public class RouteConfiguration {
@@ -19,27 +25,41 @@ public class RouteConfiguration {
     public RouteLocator routeConfig(RouteLocatorBuilder builder) {
         return builder.routes().build();
     }
-
     @Bean
     public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
         return factory -> factory.configureDefault(id ->
                 new Resilience4JConfigBuilder(id)
                         .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-                        .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(10))
+                        .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(20))
                                 .build())
                         .build());
+    }
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(10,20, 50);
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just("1");
     }
 
     @Bean
     public RouteLocator routeLocatorCircuitBreaker(RouteLocatorBuilder builder) {
 
-        return builder.routes()
+        return (RouteLocator) builder.routes()
                 .route(p -> p
                         .path("/api/v1/user/**")
+                        .and()
+                        .method(GET)
                         .filters(f -> f
                                 .circuitBreaker(config -> config
                                         .setName("userCircuitBreaker")
-                                        .setFallbackUri("forward:/api/error/fallback/contactSupport")))
+                                        .setFallbackUri("forward:/api/error/fallback/contactSupport"))
+                                .requestRateLimiter()
+                                .configure(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(userKeyResolver())))
                         .uri("lb://USER-SERVICE-MS"))
 
                 .route(p -> p
